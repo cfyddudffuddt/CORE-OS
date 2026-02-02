e2b4c95d3c0a55191676b2645e504a949318694a
Akshay Suresh <85290871+cfyddudffuddt@users.noreply.github.com>
2026-02-02 11:43:08 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// --- 1. PASTE YOUR KEYS INSIDE THIS BOX ---
 const firebaseConfig = {
   apiKey: "AIzaSyCIKoqUtc7IkUCEzDJVBDQ__tZWoAlzUa0",
   authDomain: "core-os-db.firebaseapp.com",
@@ -12,12 +7,12 @@ const firebaseConfig = {
   messagingSenderId: "291952417111",
   appId: "1:291952417111:web:eb4d651d53563091208612"
 };
-// Initialize Firebase
+
+// --- 2. INITIALIZE FIREBASE (DO NOT TOUCH THIS PART) ---
+// This uses the "firebase" global variable we loaded in index.html
 firebase.initializeApp(firebaseConfig);
 
 // Initialize Authentication
 const auth = firebase.auth();
 
 console.log("FIREBASE CONNECTED: READY");
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
